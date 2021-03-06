ab4e00dd83db71170291ba121c7835b5cf11f347
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-03-07 00:35:36 +1300

    Input text and date output. Just testing stuff

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function changeLabels(box, text){
+
+    var value = document.getElementById(text).value; 
+
+    if (value === "") {
+        alert("please enter something");
+        document.getElementById(box).textContent = "Please enter something...";
+    }
+
+    else
+    {
+        document.getElementById(box).textContent = value;
+    }
+}
+
+function printDate(box){
+    var date = new Date(Date.now())
+    document.getElementById(box).textContent = "Date: " + date.toLocaleDateString() + " - Time: " + date.toLocaleTimeString();
+}
