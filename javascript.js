35a041be1e0d59f0ec8563f75196feb00dd68413
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-03-08 14:19:53 +1300

    Organized stuff, added an image and added the alert for when a key is pressed

--- javascript.js
+++ javascript.js
@@ -6,6 +6,26 @@ function changeLabels(box, text){
         document.getElementById(box).textContent = "Please enter something...";
     }
 
+    else if (value === "this is a text message"){
+        document.getElementById(box).textContent = "yes it is.";
+    }
+
+    else if (value === "wtf why did you reply"){
+        document.getElementById(box).textContent = "because I can?";
+    }
+
+    else if (value === "stop then, just copy what i write"){
+        document.getElementById(box).textContent = "Ok I will";
+    }
+
+    else if (value === "alright so the program is working now"){
+        document.getElementById(box).textContent = "am i?";
+    }
+
+    else if (value === "shut up"){
+        document.getElementById(box).textContent = "ok ok ok"
+    }
+
     else
     {
         document.getElementById(box).textContent = value;
@@ -16,3 +36,27 @@ function printDate(box){
     var date = new Date(Date.now())
     document.getElementById(box).textContent = "Date: " + date.toLocaleDateString() + " - Time: " + date.toLocaleTimeString();
 }
+
+function resetDate(box){
+    document.getElementById(box).textContent = "Date: dd/mm/yyyy - Time: hh:mm:ss";
+}
+
+document.addEventListener('keydown', function(event) {
+
+    switch (event.keyCode) {
+        case 37:
+            alert('Left was pressed')
+            break;
+        case 38:
+            alert('Up was pressed');
+            break;
+        case 39:
+            alert('Right was pressed');
+            break;
+        case 40:
+            alert('Down was pressed');
+            break;
+        default:
+            break;
+    }
+});
